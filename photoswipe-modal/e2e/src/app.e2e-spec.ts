226c46ef89a3c60257fedb440642ce4d11cc9ffb
jexists <jexists486@gmail.com>
2021-01-19 23:12:14 +0900

    init

--- photoswipe-modal/e2e/src/app.e2e-spec.ts
+++ photoswipe-modal/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('photoswipe app is running!');
+    expect(page.getTitleText()).toEqual('photoswipe-modal app is running!');
   });
 
   afterEach(async () => {
--- photoswipe-modal/karma.conf.js
+++ photoswipe-modal/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/photoswipe'),
+      dir: require('path').join(__dirname, './coverage/photoswipe-modal'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },

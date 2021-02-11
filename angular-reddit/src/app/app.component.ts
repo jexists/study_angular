74223eaef07b2388c3d99725f0cefe5d44826475
jexists <jexists486@gmail.com>
2021-02-11 19:41:28 +0900

    code  done

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reddit';
+  articles: Article[]; //Article Array = 제네릭(generics)
 
+  constructor() {
+    this.articles = [
+      new Article('anulgar', 'http://angular.io', 3),
+      new Article('fullstack', 'http://fullstack.io', 2),
+      new Article('naver', 'http://naver.com', 1),
+    ]
+  }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(title, link);
     console.log(title.value, link.value);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article }  from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,27 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
-  votes: number;
-  link: string;
-  title: string;
+  @Input() article: Article;
 
   constructor() {
-    this.votes = 10;
-    this.link = 'http://angular.io';
-    this.title = 'angular';
+    // this.article = new Article(
+    //   'angular',
+    //   'http://angular.io',
+    //   10
+    // )
   }
 
   ngOnInit() {
   }
 
   voteUp() {
-    this.votes += 1;
+    // this.article.votes += 1;
+    this.article.voteUp();
     // return false;
   }
   
   voteDown() {
-    this.votes -= 1;
+    // this.article.votes -= 1;
+    this.article.voteDown();
     // return false;
   }
-
+  
 }

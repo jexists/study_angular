841bb7ac674883e0cbead1f3c8089d816b82f4f1
jexists <jexists486@gmail.com>
2021-04-21 21:47:26 +0900

    formsModule

--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -7,18 +7,23 @@ import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { PriceDisplayComponent } from './price-display/price-display.component';
 import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     ProductListComponent,
     ProductRowComponent,
     ProductImageComponent,
     PriceDisplayComponent,
-      ProductDepartmentComponent
-   ],
+    ProductDepartmentComponent,
+    DemoFormSkuComponent,
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

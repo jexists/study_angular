5955c5f91c4fa8e972034d5b9ef5efca3838cd4d
jexists <jexists486@gmail.com>
2022-01-25 22:52:37 +0900

    structural directive

--- directive/src/app/app-routing.module.ts
+++ directive/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-   declarations: [
-    "HighlightDirective"
-  ]
+   declarations: [	
+    "HighlightDirective",
+      UnlessDirective
+   ]
 })
 export class AppRoutingModule { }

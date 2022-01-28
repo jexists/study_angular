49c1bd3f8aba87ecd4bf11b4726b9270182a13ea
jexists <jexists486@gmail.com>
2022-01-28 22:40:34 +0900

    사용자 정의 파이프

--- pipe/src/app/app-routing.module.ts
+++ pipe/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyPipePipe } from './my-pipe.pipe';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+   declarations: [
+    "MyPipePipe"
+  ]
 })
 export class AppRoutingModule { }
--- pipe/src/app/app.module.ts
+++ pipe/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyPipePipe } from './my-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyPipePipe
   ],
   imports: [
     BrowserModule,

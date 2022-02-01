b1b1681480294bfd14af3d4481bde9fc5f4fb637
jexists <jexists486@gmail.com>
2022-02-01 20:26:08 +0900

    binding

--- udemy-basic/src/app/servers/servers.component.ts
+++ udemy-basic/src/app/servers/servers.component.ts
@@ -16,6 +16,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,11 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+
+  this.serverCreationStatus = 'Server was created!';
   }
 
 }

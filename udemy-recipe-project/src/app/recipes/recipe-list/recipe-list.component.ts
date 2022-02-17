3e8413adb461197531ba3b6318f95fb8a39ce878
jexists <jexists486@gmail.com>
2022-02-17 23:27:23 +0900

    recipe detail

--- udemy-recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', '')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://m.6recipe.co.kr/postfixo/images/gamre_m.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://m.6recipe.co.kr/postfixo/images/gamre_m.jpg')
   ];
 
   constructor() { }
